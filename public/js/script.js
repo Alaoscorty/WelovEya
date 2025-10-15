73b74d4342633d6ed4f41112541184e96967d7c8
Alaoscorty <133812869+Alaoscorty@users.noreply.github.com>
2025-10-15 14:17:45 +0100

    mise à jour du code des cartes

--- public/js/script.js
+++ public/js/script.js
@@ -39,8 +39,34 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
+//script pour gérer le filtre d'évènement 
+document.addEventListener('DOMContentLoaded', function() {
+    const filterButtons = document.querySelectorAll('.filter-btn');
+    const cards = document.querySelectorAll('.card');
+
+    filterButtons.forEach(btn => {
+        btn.addEventListener('click', () => {
+            const filter = btn.getAttribute('data-filter');
+            filterButtons.forEach(b => {
+                b.classList.remove('bg-orange-500');
+                b.classList.add('bg-gray-700');
+            });
+            btn.classList.remove('bg-gray-700');
+            btn.classList.add('bg-orange-500');
+
+            cards.forEach(card => {
+                if (filter === 'all' || card.getAttribute('data-genre') === filter) {
+                    card.style.display = 'block';
+                } else {
+                    card.style.display = 'none';
+                }
+            });
+        });
+    });
+});   
 //script pour la gestion du carousel
 document.addEventListener('DOMContentLoaded', function() {
+
 
     feather.replace();
 
