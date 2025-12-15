10720e3aa61e76609472fa4b654e70b7d6539be2
Alaoscorty <133812869+Alaoscorty@users.noreply.github.com>
2025-12-15 09:46:29 +0100

    nouvelle mise à jour

---
+++ resources/views/dashboard/billet_streaming.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.application')
+
+@section('title', 'Artistes')
+<style>
+    
+</style>
+@section('content')
+
+    {{-- MAIN CONTENT --}}
+    
+@endsection
+    
+    <script>
+        
+    </script>
+    @push('scripts')
+@endpush
